0aff6cfcd2770e564c932b8c428745b736fcc73d
Abdhullah Ariff <abdhullahariff118@gmail.com>
2026-06-14 19:33:43 +0530

    Initial commit

---
+++ Lume/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lume.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
