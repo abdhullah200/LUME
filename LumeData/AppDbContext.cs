a2829d8be6a5ffe31f13c2488aae16646d9a8025
Abdhullah Ariff <abdhullahariff118@gmail.com>
2026-08-17 22:40:29 +0530

    DataBase Step up, migation and Creating a new project for Database

---
+++ LumeData/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Lume.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lume.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Post> Posts { get; set; } 
+    }
+}
