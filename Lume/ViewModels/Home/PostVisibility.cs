26f0bb033c138a34e1769402058ab2d9b7d05bf6
Abdhullah Ariff <abdhullahariff118@gmail.com>
2026-09-06 20:44:44 +0530

    save post impeletion

---
+++ Lume/ViewModels/Home/PostVisibility.cs
@@ -0,0 +1,10 @@
+﻿namespace Lume.ViewModels.Home
+{
+    public class PostVisibility
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the post whose visibility is being managed.
+        /// </summary>
+        public int PostId { get; set; }
+    }
+}
---
+++ LumeData/Migrations/20260906131749_Added_Post_Favorites.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lume.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Post_Favorites : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Favorites",
+                columns: table => new
+                {
+                    postId = table.Column<int>(type: "int", nullable: false),
+                    userId = table.Column<int>(type: "int", nullable: false),
+                    id = table.Column<int>(type: "int", nullable: false),
+                    dateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Favorites", x => new { x.postId, x.userId });
+                    table.ForeignKey(
+                        name: "FK_Favorites_Posts_postId",
+                        column: x => x.postId,
+                        principalTable: "Posts",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Favorites_Users_userId",
+                        column: x => x.userId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Favorites_userId",
+                table: "Favorites",
+                column: "userId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Favorites");
+        }
+    }
+}
